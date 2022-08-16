2334e8d51ee75e307601fe7d3da2c72f6e2e8fe9
Lucas <lucaslepsg@gmail.com>
2022-08-15 21:16:09 -0300

    Primeiro commit

---
+++ NotasEscolares.js
@@ -0,0 +1,28 @@
+
+function getScore(score){
+let scoreA = score >=90 && score <=100
+let scoreB = score >=80 && score <= 89 
+let scoreC = score >=70 && score <= 79 
+let scoreD = score >=60 && score <= 69 
+let scoreF = score < 60 && score >=0
+let scoreFinal;
+if (scoreA){
+    scoreFinal ="A"
+} else if (scoreB){
+    scoreFinal ="B"
+} else if (scoreC){
+    scoreFinal ="C"
+} else if (scoreD){
+    scoreFinal ="D"
+} else if (scoreF){
+    scoreFinal ="F"
+} else {
+    scoreFinal ="Nova Inválida"
+}
+return scoreFinal
+}
+
+console.log(getScore(50))
+console.log(getScore(90))
+console.log(getScore(-5))
+console.log(getScore(60))
---
+++ script.js
@@ -0,0 +1,15 @@
+
+function frases(){
+  console.log('Estudar é brabo')
+  console.log('Estudar é brabo')
+  console.log('Estudar é brabo')
+}
+frases()
+frases()
+frases()
+frases()
+
+
+
+
+
